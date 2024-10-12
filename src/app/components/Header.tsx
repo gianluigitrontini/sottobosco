6d2d21456355d5157768f26434a92218214b76fa
gianluigitrontini <gianluigitrontini@gmail.com>
2024-10-12 17:05:03 +0200

    Implementa hamburger menu.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -18,6 +19,10 @@ const Header = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <header
       className={`z-10 fixed top-0 w-full transition duration-500 border-b ${
@@ -28,7 +33,7 @@ const Header = () => {
     >
       <div className="container flex justify-between items-center">
         <img src="/logo.png" alt="" className="py-4 max-w-[100px] w-full" />
-        <nav>
+        <nav className="hidden md:block">
           <ul className="flex gap-4">
             <li>
               <a href="#chi-siamo">Chi Siamo</a>
@@ -47,7 +52,63 @@ const Header = () => {
             </li>
           </ul>
         </nav>
+        {/* Hamburger menu icon */}
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="focus:outline-none">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </div>
+
+      {menuOpen && (
+        <nav className="md:hidden w-full fixed inset-0 bg-white">
+          <ul className="flex flex-col gap-8 p-4 text-xl text-center">
+            <li>
+              <a href="#chi-siamo" onClick={() => toggleMenu()}>
+                Chi Siamo
+              </a>
+            </li>
+            <li>
+              <a href="#marketplace" onClick={() => toggleMenu()}>
+                Marketplace
+              </a>
+            </li>
+            <li>
+              <a href="#produttori" onClick={() => toggleMenu()}>
+                Per i Produttori
+              </a>
+            </li>
+            <li>
+              <a href="#corsi-e-laboratori" onClick={() => toggleMenu()}>
+                Corsi e Laboratori
+              </a>
+            </li>
+            <li>
+              <a href="#comunita" onClick={() => toggleMenu()}>
+                Rimani Aggiornato
+              </a>
+            </li>
+            <li>
+              <button onClick={() => toggleMenu()} className="btn-outline">
+                Chiudi
+              </button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
